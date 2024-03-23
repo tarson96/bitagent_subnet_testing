bb052e2a454965628847380ce7b04cf5cfefd244
tarson96 <tarunsoni@taruns-MacBook-Air.local>
2024-03-23 18:44:00 +0530

    changes

--- bitagent/miners/t5_miner.py
+++ bitagent/miners/t5_miner.py
@@ -27,9 +27,9 @@ import torch
 
 def miner_init(self, config=None):
     transformers.logging.set_verbosity_error()
-    print("Model being used --- ", "microsoft/phi-2")
-    self.tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
-    self.model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+    print("Model being used --- ", "mosaicml/mpt-7b")
+    self.tokenizer = AutoTokenizer.from_pretrained("mosaicml/mpt-7b", trust_remote_code=True)
+    self.model = AutoModelForCausalLM.from_pretrained("mosaicml/mpt-7b", trust_remote_code=True)
     device = torch.device("cuda")
     self.model.to(device)
 
