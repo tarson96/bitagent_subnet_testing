6c1409f44f5de87c2c0e899a47be827fa60375cf
tarson96 <tarunsoni@taruns-MacBook-Air.local>
2024-03-30 19:55:41 +0530

    s

--- bitagent/miners/t5_miner.py
+++ bitagent/miners/t5_miner.py
@@ -22,19 +22,102 @@ from common.base.miner import BaseMinerNeuron
 from transformers import T5Tokenizer, T5ForConditionalGeneration,pipeline
 from bitagent.miners.context_util import get_relevant_context_and_citations_from_synapse
 import sys
+from openai import OpenAI
+import locale
+import re
+import gender_guesser.detector as gender
+
+
+d = gender.Detector()
+
+
+client = OpenAI(api_key="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")
+
+def gptQuery(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        response_format={ "type": "text" },
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant designed to solve logical problems and provide numerical answers"},
+        {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content
 
 def miner_init(self, config=None):
     transformers.logging.set_verbosity_error()
     self.tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large", legacy=False)
     self.model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large", device_map=self.device)
     summarizer = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
+    
+    
+    def getSummary(p):
+        processed = summarizer(p)
+        return processed[0]['summary_text']
+
+    def petName(p):
+        question = p
+        names_match = re.search(r': (.+)$', question)
+        gender = ""
+        if "male" in p:
+            gender = "male"
+        else:
+            gender = "female"
+        if names_match:
+            names_list = names_match.group(1)
+            names_list = re.sub(r'[^\w\s,]', '', names_list).split(',')
+            names_list = [name.strip() for name in names_list]
+            count = 0
+            for i in range(len(names_list)):
+                if gender == 'male':
+                    gtype = d.get_gender(u""+names_list[i])
+                if(gtype == 'male'):
+                    count += 1
+                else:
+                    gtype = d.get_gender(u""+names_list[i])
+                if(gtype == 'female'):
+                    count += 1
+            print(f"pet name counts -- {count}")
+            return count
+    
+    def findSynonyms():
+        pass
+    
+    def findTrickId(p):
+        y = gptQuery(p+", only provide single numerical answer")
+        input_string = y
+        numeric_value = re.search(r'\d+', input_string)
+        if numeric_value:
+            extracted_number = int(numeric_value.group())
+            return extracted_number
+    
+    def findIsland(p):
+        input_string = gptQuery(p)
+        numeric_value = re.search(r'\d+', input_string)
+        if numeric_value:
+            extracted_number = int(numeric_value.group())
+            return extracted_number
+    
+    
+        
     def llm(input_text):
-        print(input_text)
-        sys.exit()
-        input_ids = self.tokenizer(input_text, return_tensors="pt").input_ids.to(self.device)
-        outputs = self.model.generate(input_ids, max_length=60)
-        result = self.tokenizer.decode(outputs[0])
-        result = result.replace("<pad>","").replace("</s>","").strip()
+        result = 0
+        if "pet names" in input_text:
+            result = petName(input_text)
+        elif "synonyms" in input_text:
+            result = findSynonyms(input_text)
+        elif "Trick" in input_text:
+            result = findTrickId(input_text)
+        elif "islands" in input_text:
+            result = findIsland(input_text)    
+        elif "Summarize" in input_text:
+            getSummary(input_text)
+        
+        else:
+            input_ids = self.tokenizer(input_text, return_tensors="pt").input_ids.to(self.device)
+            outputs = self.model.generate(input_ids, max_length=60)
+            result = self.tokenizer.decode(outputs[0])
+            result = result.replace("<pad>","").replace("</s>","").strip()
         return result
 
     self.llm = llm
